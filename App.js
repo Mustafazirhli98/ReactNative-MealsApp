29a7e755f3639f761a313ce3776594618d6bbafc
Mustafa Zırhlı <mustafazirhli98@gmail.com>
2024-03-07 16:35:08 +0300

    edited category screen and added to stackNavigator

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import CategoryScreen from './screens/CategoryScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator();
+
   return (
     <>
-      <CategoryScreen />
       <StatusBar style="auto" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='CategoryScreen' component={CategoryScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
